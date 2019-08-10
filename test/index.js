b2312925e2638385f1ec87ca4034c7759d805585
Jason Jacob <jason.jacob@safetyculture.io>
2019-08-10 15:59:55 +0930

    update deps

--- test/index.js
+++ test/index.js
@@ -1,45 +1,45 @@
-import {assert} from 'chai';
+import assert from 'assert';
 import {languagePriorityList, basicFilter, lookup, extendedFilter} from '../src/index';
 
 describe('accept-language-node', () => {
   describe('languagePriorityList', () => {
-    it('should be an function', () => {
-      assert.typeOf(languagePriorityList, 'function', 'should be a function');
+    it('should be an Function', () => {
+      assert(typeof languagePriorityList === 'function', 'should be a Function');
     });
 
     it('should assign default quality if languagePriorityList quality fails', () => {
-      assert.deepEqual(languagePriorityList('en;q0.1'), [{ tag: 'en', quality: 1 }],
+      assert.deepStrictEqual(languagePriorityList('en;q0.1'), [{ tag: 'en', quality: 1 }],
         'expected to languagePriorityList single tag');
     });
 
     it('should assign default quality if languagePriorityList quality is higher than 1', () => {
-      assert.deepEqual(languagePriorityList('en;q=undefined'), [{ tag: 'en', quality: 1 }],
+      assert.deepStrictEqual(languagePriorityList('en;q=undefined'), [{ tag: 'en', quality: 1 }],
         'expected to languagePriorityList single tag');
     });
 
     it('should assign default quality if languagePriorityList quality is higher than 1', () => {
-      assert.deepEqual(languagePriorityList('en;q=1.1'), [{tag: 'en', quality: 1}],
+      assert.deepStrictEqual(languagePriorityList('en;q=1.1'), [{tag: 'en', quality: 1}],
         'expected to languagePriorityList single tag');
     });
 
     it('should return wildcard when given value other than a string', () => {
-      assert.deepEqual(languagePriorityList(undefined), [{tag: '*', quality: 1}], '* obj expected');
-      assert.deepEqual(languagePriorityList(null), [{tag: '*', quality: 1}], '* obj expected');
-      assert.deepEqual(languagePriorityList(1), [{tag: '*', quality: 1}], '* obj expected');
-      assert.deepEqual(languagePriorityList(true), [{tag: '*', quality: 1}], '* obj expected');
-      assert.deepEqual(languagePriorityList({}), [{tag: '*', quality: 1}], '* obj expected');
-      assert.deepEqual(languagePriorityList([]), [{tag: '*', quality: 1}], '* obj expected');
+      assert.deepStrictEqual(languagePriorityList(undefined), [{tag: '*', quality: 1}], '* obj expected');
+      assert.deepStrictEqual(languagePriorityList(null), [{tag: '*', quality: 1}], '* obj expected');
+      assert.deepStrictEqual(languagePriorityList(1), [{tag: '*', quality: 1}], '* obj expected');
+      assert.deepStrictEqual(languagePriorityList(true), [{tag: '*', quality: 1}], '* obj expected');
+      assert.deepStrictEqual(languagePriorityList({}), [{tag: '*', quality: 1}], '* obj expected');
+      assert.deepStrictEqual(languagePriorityList([]), [{tag: '*', quality: 1}], '* obj expected');
     });
 
     it('should split range string correctly into objects', () => {
-      assert.deepEqual(languagePriorityList('*'), [{tag: '*', quality: 1}], 'expected to languagePriorityList');
-      assert.deepEqual(languagePriorityList('en'), [{tag: 'en', quality: 1}], 'expected to languagePriorityList');
-      assert.deepEqual(languagePriorityList('en-US'), [{tag: 'en-US', quality: 1}], 'expected to languagePriorityList');
+      assert.deepStrictEqual(languagePriorityList('*'), [{tag: '*', quality: 1}], 'expected to languagePriorityList');
+      assert.deepStrictEqual(languagePriorityList('en'), [{tag: 'en', quality: 1}], 'expected to languagePriorityList');
+      assert.deepStrictEqual(languagePriorityList('en-US'), [{tag: 'en-US', quality: 1}], 'expected to languagePriorityList');
 
-      assert.deepEqual(languagePriorityList('zh-Hant-CN-x-private1-private2'), [{tag: 'zh-Hant-CN-x-private1-private2',
+      assert.deepStrictEqual(languagePriorityList('zh-Hant-CN-x-private1-private2'), [{tag: 'zh-Hant-CN-x-private1-private2',
         quality: 1}], 'expected to correctly languagePriorityList private into valid tag objects');
 
-      assert.deepEqual(languagePriorityList('en-GB,en-US;q=0.7,fr-CA;q=0.8,en;q=0.5, *'), [
+      assert.deepStrictEqual(languagePriorityList('en-GB,en-US;q=0.7,fr-CA;q=0.8,en;q=0.5, *'), [
         { tag: '*', quality: 1 },
         { tag: 'en-GB', quality: 1 },
         { tag: 'fr-CA', quality: 0.8 },
@@ -49,7 +49,7 @@ describe('accept-language-node', () => {
     });
 
     it('should return sorted in order quality, tag specificity from high to low', () => {
-      assert.deepEqual(languagePriorityList('en-GB,en-US;q=0.7,zh-Hant-CN;q=0.8,fr-CA;q=0.8,en;q=0.5'), [
+      assert.deepStrictEqual(languagePriorityList('en-GB,en-US;q=0.7,zh-Hant-CN;q=0.8,fr-CA;q=0.8,en;q=0.5'), [
         { tag: 'en-GB', quality: 1 },
         { tag: 'zh-Hant-CN', quality: 0.8},
         { tag: 'fr-CA', quality: 0.8 },
@@ -59,7 +59,7 @@ describe('accept-language-node', () => {
     });
 
     it('wild card tag should always be sorted to first position', () => {
-      assert.deepEqual(languagePriorityList('en-GB,en-US;q=0.7,*,zh-Hant-CN;q=0.8,fr-CA;q=0.8,en;q=0.5'), [
+      assert.deepStrictEqual(languagePriorityList('en-GB,en-US;q=0.7,*,zh-Hant-CN;q=0.8,fr-CA;q=0.8,en;q=0.5'), [
         { tag: '*', quality: 1 },
         { tag: 'en-GB', quality: 1 },
         { tag: 'zh-Hant-CN', quality: 0.8},
@@ -68,7 +68,7 @@ describe('accept-language-node', () => {
         { tag: 'en', quality: 0.5 }
       ], 'expected to correctly languagePriorityList range into valid tag objects');
 
-      assert.deepEqual(languagePriorityList('*,en-GB,en-US;q=0.7,fr-CA;q=0.8,en;q=0.5'), [
+      assert.deepStrictEqual(languagePriorityList('*,en-GB,en-US;q=0.7,fr-CA;q=0.8,en;q=0.5'), [
         {
           quality: 1,
           tag: '*'
@@ -94,58 +94,58 @@ describe('accept-language-node', () => {
   });
 
   describe('basicFilter', () => {
-    it('should be an function', () => {
-      assert.typeOf(basicFilter, 'function', 'should be a function');
+    it('should be an Function', () => {
+      assert(typeof basicFilter === 'function', 'should be a Function');
     });
 
     it('should return an empty array if languageTags are not given', () => {
-      assert.deepEqual(basicFilter('en'), [], 'expected empty array to be returned');
+      assert.deepStrictEqual(basicFilter('en'), [], 'expected empty array to be returned');
     });
 
     it('should return an empty array if no match is found', () => {
-      assert.deepEqual(basicFilter('en-GB,en-US;q=0.7,fr-CA;q=0.8,en;q=0.5', ['ru', 'it']), [],
+      assert.deepStrictEqual(basicFilter('en-GB,en-US;q=0.7,fr-CA;q=0.8,en;q=0.5', ['ru', 'it']), [],
         'expected empty array to be returned if no match is made');
     });
 
     it('should match all when wildcard is present', () => {
-      assert.deepEqual(basicFilter('*,en-GB,en-US;q=0.7,fr-CA;q=0.8,en;q=0.5, ',
+      assert.deepStrictEqual(basicFilter('*,en-GB,en-US;q=0.7,fr-CA;q=0.8,en;q=0.5, ',
         ['zh-Hant-CN-x-private1-private2', 'en', 'zh']), ['zh-Hant-CN-x-private1-private2', 'en', 'zh'],
         'should return all matches if wild card is present');
     });
 
     it('should match when priority tag and language tag exactly match', () => {
-      assert.deepEqual(basicFilter('zh-Hant-CN-x-private1-private2', ['zh-Hant-CN-x-private1-private2']),
+      assert.deepStrictEqual(basicFilter('zh-Hant-CN-x-private1-private2', ['zh-Hant-CN-x-private1-private2']),
         ['zh-Hant-CN-x-private1-private2'], 'should match');
     });
 
     it('should match prefix correctly when direct match can not be made', () => {
-      assert.deepEqual(basicFilter('de-de', ['de-DE', 'de-DE-1996', 'de-Deva', 'de-Latn-DE']),
+      assert.deepStrictEqual(basicFilter('de-de', ['de-DE', 'de-DE-1996', 'de-Deva', 'de-Latn-DE']),
         ['de-DE', 'de-DE-1996'], 'should match');
     });
 
     it('should match all when only language is given in language range', () => {
-      assert.deepEqual(basicFilter('de', ['de-DE', 'de-DE-1996', 'de-Deva', 'de-Latn-DE']),
+      assert.deepStrictEqual(basicFilter('de', ['de-DE', 'de-DE-1996', 'de-Deva', 'de-Latn-DE']),
         ['de-DE', 'de-DE-1996', 'de-Deva', 'de-Latn-DE'], 'should match');
     });
 
     it('should return results in Priority List order', () => {
-      assert.deepEqual(basicFilter('en-GB,en-US;q=0.7,zh-Hant-CN-x-private1-private2;q=0.8,fr-CA;q=0.8,en;q=0.5',
+      assert.deepStrictEqual(basicFilter('en-GB,en-US;q=0.7,zh-Hant-CN-x-private1-private2;q=0.8,fr-CA;q=0.8,en;q=0.5',
         ['en-GB', 'en-US', 'zh-Hant-CN-x-private1-private2', 'fr-CA', 'en']),
         ['en-GB', 'zh-Hant-CN-x-private1-private2', 'fr-CA', 'en-US', 'en'], 'should be in order');
     });
   });
 
   describe('extendedFilter', () => {
-    it('should be an function', () => {
-      assert.typeOf(extendedFilter, 'function', 'should be a function');
+    it('should be an Function', () => {
+      assert(typeof extendedFilter === 'function', 'should be a Function');
     });
 
     it('should return an empty array if languageTags are not given', () => {
-      assert.deepEqual(extendedFilter('en'), [], 'expected empty array to be returned');
+      assert.deepStrictEqual(extendedFilter('en'), [], 'expected empty array to be returned');
     });
 
     it('should match extended tag correctly as described in RFC 3.3.2', () => {
-      assert.deepEqual(extendedFilter('de-*-DE', [
+      assert.deepStrictEqual(extendedFilter('de-*-DE', [
         // testing
         'ru',               // should fail
         'de',               // should fail
@@ -170,12 +170,12 @@ describe('accept-language-node', () => {
   });
 
   describe('lookup', () => {
-    it('should be an function', () => {
-      assert.typeOf(lookup, 'function', 'should be a function');
+    it('should be an Function', () => {
+      assert(typeof lookup === 'function', 'should be a Function');
     });
 
     it('should throw if correct args are not supplied', () => {
-      assert.throws(() => lookup('stuff'), 'range:String, languageTags:Array, defaultValue:String required!',
+      assert.throws(() => lookup('stuff'), Error('range:String, languageTags:Array, defaultValue:String required!'),
         'should throw error if arguments are not supplied');
     });
 
